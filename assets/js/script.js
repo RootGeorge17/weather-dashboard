0cf205cd287d20295860c2eb546e7df6cf9318ec
RootGeorge <g.sopov@edu.salford.ac.uk>
2024-03-08 17:54:14 +0000

    Fetched today's weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-// Get references to DOM elements
 var searchButton = document.getElementById("search-button");
 var searchInput = document.getElementById("search-input");
+var APItoken = "604a01f8b6790b14c2c34ec56f00eaa6";
 
 // Register event listener with callback function citySearch
 searchButton.addEventListener("click", citySearch);
@@ -9,13 +9,55 @@ function citySearch(event) {
   event.preventDefault();
 
   var city = searchInput.value.trim();
+  searchInput.value = "";
+
   // Maybe validate only for valid cities next
   if (city) {
     setNewCityToLocalStorage(city);
 
     // Rendering Storage after adding new city
     renderStorage();
+    renderCurrentWeather(city);
   }
+}
+
+function renderCurrentWeather(city) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=metric" +
+    "&appid=" +
+    APItoken;
+
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // Transfer content to HTML
+      var weatherToday = document.getElementById("today");
+      weatherToday.innerHTML = "<h2>" + city;
+
+      var tempCelsius = document.createElement("p");
+      var tempInCelsius = data.main.temp;
+      tempCelsius.textContent = "Temp: " + tempInCelsius.toFixed(2) + "°C";
+
+      var wind = document.createElement("p");
+      var windInKPH = data.wind.speed * 3.6;
+      wind.textContent = "Wind: " + windInKPH.toFixed(2) + " KPH";
+
+      var humidity = document.createElement("p");
+      humidity.textContent = "Humidity: " + data.main.humidity + "%";
+
+      // Append to the HTML container
+      weatherToday.appendChild(tempCelsius);
+      weatherToday.appendChild(wind);
+      weatherToday.appendChild(humidity);
+    })
+    .catch(function (error) {
+      console.error("Error fetching data:", error);
+    });
 }
 
 function renderStorage() {
