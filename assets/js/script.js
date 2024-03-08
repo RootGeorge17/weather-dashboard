0fbf0ee231f8f497af8bbf92db338eb5f9e32d87
RootGeorge <g.sopov@edu.salford.ac.uk>
2024-03-08 17:17:00 +0000

    Rendering of local storage on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,34 @@
 var searchButton = document.getElementById("search-button");
 var searchInput = document.getElementById("search-input");
 
-function citySearch() {
+// Register event listener with callback function citySearch
+searchButton.addEventListener("click", citySearch);
+
+function citySearch(event) {
+  event.preventDefault();
+
   var city = searchInput.value.trim();
   // Maybe validate only for valid cities next
   if (city) {
     setNewCityToLocalStorage(city);
+
+    // Rendering Storage after adding new city
+    renderStorage();
   }
+}
+
+function renderStorage() {
+  var citiesList = document.getElementById("history");
+  citiesList.innerHTML = "";
+
+  var cities = getCitiesFromLocalStorage();
+
+  cities.forEach((city) => {
+    var listCity = document.createElement("button");
+    listCity.textContent = city;
+    listCity.classList.add("city-button");
+    citiesList.appendChild(listCity);
+  });
 }
 
 // Function to retrieve cities from local storage
@@ -23,5 +45,5 @@ function setNewCityToLocalStorage(city) {
   localStorage.setItem("cities", JSON.stringify(cities));
 }
 
-// Register event listener with callback function citySearch
-searchButton.addEventListener("click", citySearch);
+// Initial rendering of storage
+renderStorage();
